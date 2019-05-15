7cd1b8a450c42f11404ff1ca88a3a8e83b96f290
moses <moseslee90@gmail.com>
2019-05-15 21:05:53 +0800

    MVP: changed file to jsx, changed changeHandler to an arrow function, added clickHandler for button, made state change for input and adding of ListItem onClick work

---
+++ script.jsx
@@ -0,0 +1,49 @@
+class List extends React.Component {
+    constructor() {
+        super();
+    }
+
+    state = {
+        list: [],
+        word: ""
+    };
+
+    changeHandler = event => {
+        this.setState({
+            word: event.target.value
+        });
+        console.log("change", event.target.value);
+    };
+
+    clickHandler = () => {
+        let newList = this.state.list;
+        let newKey = newList.length;
+        console.log(newList);
+        newList.push(<ListItem key={newKey} item={this.state.word} />);
+        this.setState({
+            list: newList
+        });
+    };
+
+    render() {
+        // render the list with a map() here
+        let listItems = this.state.list;
+        console.log("rendering");
+        return (
+            <div className="list">
+                <input onChange={this.changeHandler} value={this.state.word} />
+                <button onClick={this.clickHandler}>add item</button>
+                <ul>{listItems}</ul>
+            </div>
+        );
+    }
+}
+
+class ListItem extends React.Component {
+    render() {
+        let item = this.props.item;
+        return <li>{item}</li>;
+    }
+}
+
+ReactDOM.render(<List />, document.getElementById("root"));

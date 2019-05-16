78f5ce66d3b9dc9033798454f9bf647f50863394
moses <moseslee90@gmail.com>
2019-05-16 23:40:45 +0800

    successfully added delete button that deletes correct listItem

--- script.jsx
+++ script.jsx
@@ -4,7 +4,8 @@ class App extends React.Component {
     }
     state = {
         list: [],
-        word: ""
+        word: "",
+        keyCount: 0
     };
 
     changeHandler = event => {
@@ -19,12 +20,35 @@ class App extends React.Component {
         if (word.length > 1 && word.length < 100) {
             let newList = this.state.list;
             //allow click functions
-            let newKey = newList.length;
+            let newKey = this.state.keyCount;
+            this.state.keyCount++;
+            newList.push(
+                <ListItem
+                    listid={newKey}
+                    key={newKey}
+                    item={this.state.word}
+                    deleteItem={this.deleteItem}
+                />
+            );
             console.log(newList);
-            newList.push(<ListItem key={newKey} item={this.state.word} />);
             this.setState({
                 list: newList
             });
+        }
+    };
+
+    deleteItem = event => {
+        //removes this List Item in list with the particular key
+        let list = this.state.list;
+        let itemId = parseInt(event.target.attributes.listid.value);
+        for (let index = 0; index < list.length; index++) {
+            const element = list[index];
+            if (itemId === element.props.listid) {
+                list.splice(index, 1);
+                this.setState({
+                    list: list
+                });
+            }
         }
     };
 
@@ -64,7 +88,16 @@ class ItemList extends React.Component {
 class ListItem extends React.Component {
     render() {
         let item = this.props.item;
-        return <li>{item}</li>;
+        return (
+            <div>
+                <li>{item}</li>
+                <button
+                    onClick={this.props.deleteItem}
+                    listid={this.props.listid}>
+                    delete
+                </button>
+            </div>
+        );
     }
 }
 

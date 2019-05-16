a3496009f7238145b3472c20a9b8f5497c3c5bbb
moses <moseslee90@gmail.com>
2019-05-17 00:33:29 +0800

    created map variant of this.state.list and enabled deletion of items in the map as well, solution seems better as we can simply use the reference of a key to delete a map object instead of looping through an array

--- script.jsx
+++ script.jsx
@@ -4,6 +4,7 @@ class App extends React.Component {
     }
     state = {
         list: [],
+        listMap: {},
         word: "",
         keyCount: 0
     };
@@ -31,6 +32,15 @@ class App extends React.Component {
                 />
             );
             console.log(newList);
+            let listMap = this.state.listMap;
+            listMap[newKey] = (
+                <ListItemMap
+                    listid={newKey}
+                    key={newKey}
+                    item={this.state.word}
+                    deleteItem={this.deleteItemMap}
+                />
+            );
             this.setState({
                 list: newList
             });
@@ -51,6 +61,15 @@ class App extends React.Component {
             }
         }
     };
+    deleteItemMap = event => {
+        //delete instructions for map variant
+        let itemId = parseInt(event.target.attributes.listid.value);
+        let listMap = this.state.listMap;
+        delete listMap[itemId];
+        this.setState({
+            listMap: listMap
+        });
+    };
 
     render() {
         return (
@@ -60,6 +79,8 @@ class App extends React.Component {
                     clickHandler={this.clickHandler}
                 />
                 <ItemList list={this.state.list} />
+                <h1>other list</h1>
+                <ItemListMap list={this.state.listMap} />
             </div>
         );
     }
@@ -85,7 +106,30 @@ class ItemList extends React.Component {
     }
 }
 
+class ItemListMap extends React.Component {
+    render() {
+        let listItemsMap = this.props.list;
+        let listItems = Object.values(listItemsMap);
+        return <ul>{listItems}</ul>;
+    }
+}
+
 class ListItem extends React.Component {
+    render() {
+        let item = this.props.item;
+        return (
+            <div>
+                <li>{item}</li>
+                <button
+                    onClick={this.props.deleteItem}
+                    listid={this.props.listid}>
+                    delete
+                </button>
+            </div>
+        );
+    }
+}
+class ListItemMap extends React.Component {
     render() {
         let item = this.props.item;
         return (
